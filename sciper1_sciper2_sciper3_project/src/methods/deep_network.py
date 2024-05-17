edfebf4084ec57388a5e0eb917b0b7433749c761
Ryad2 <ryad.aouak@gmail.com>
2024-05-17 17:12:55 +0200

    end of deep_network.py without trainer

--- sciper1_sciper2_sciper3_project/src/methods/deep_network.py
+++ sciper1_sciper2_sciper3_project/src/methods/deep_network.py
@@ -27,7 +27,10 @@ class MLP(nn.Module):
         super().__init__()
         ##
         ###
-        #### WRITE YOUR CODE HERE!
+        self.fc1 = nn.Linear(input_size, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, n_classes)
         ###
         ##
 
@@ -43,10 +46,14 @@ class MLP(nn.Module):
         """
         ##
         ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
-        return preds
+        pred = x.flatten(-3)
+
+        pred = F.relu(self.fc1(pred))
+        pred = F.relu(self.fc2(pred))
+        pred = F.relu(self.fc3(pred))
+        pred = self.fc4(pred)
+
+        return pred
 
 
 class CNN(nn.Module):
@@ -56,7 +63,7 @@ class CNN(nn.Module):
     It should use at least one convolutional layer.
     """
 
-    def __init__(self, input_channels, n_classes):
+    def __init__(self, input_channels, n_classes, filters=(16, 32, 64)):
         """
         Initialize the network.
         
@@ -70,7 +77,11 @@ class CNN(nn.Module):
         super().__init__()
         ##
         ###
-        #### WRITE YOUR CODE HERE!
+        self.conv2d1 = nn.Conv2d(input_channels, filters[0], 3, 1, padding=1)
+        self.conv2d2 = nn.Conv2d(filters[0], filters[1], 3, 1, padding=1)
+        self.conv2d3 = nn.Conv2d(filters[1], filters[2], 3, 1, padding=1)
+        self.fc1 = nn.Linear(3 * 3 * filters[2], 128)
+        self.fc2 = nn.Linear(128, n_classes)
         ###
         ##
 
@@ -86,11 +97,104 @@ class CNN(nn.Module):
         """
         ##
         ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
-        return preds
+        pred = F.max_pool2d(F.relu(self.conv2d1(x)), 2)
+        pred = F.max_pool2d(F.relu(self.conv2d2(pred)), 2)
+        pred = F.max_pool2d(F.relu(self.conv2d3(pred)), 2)
+        pred = pred.reshape((pred.shape[0], -1))
+        pred = F.relu(self.fc1(pred))
+        pred = self.fc2(pred)
 
+        return pred
+
+def patchify(images, n_patches):
+    n, c, h, w = images.shape
+
+    assert h == w # We assume square image.
+
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+
+                # Extract the patch of the image.
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+
+                # Flatten the patch and store it.
+                patches[idx, i * n_patches + j] = patch.flatten()
+
+    return patches
+
+def get_positional_embeddings(sequence_length, d):
+    result = torch.ones(sequence_length, d)
+    for i in range(sequence_length):
+        for j in range(d):
+            tens = torch.tensor(i / (10000 ** (2*(j//2) / d)))
+            if j % 2 == 0:
+                result[i, j] = torch.sin(tens)
+            else:
+                result[i, j] = torch.cos(tens)
+        return result
+
+class MyMSA(nn.Module):
+    def __init__(self, d, n_heads=2):
+        super(MyMSA, self).__init__()
+        self.d = d
+        self.n_heads = n_heads
+
+        assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+        d_head = int(d / n_heads)
+        self.d_head = d_head
+
+        self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, sequences):
+        result = []
+        for sequence in sequences:
+            seq_result = []
+            for head in range(self.n_heads):
+
+                # Select the mapping associated to the given head.
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
+
+                seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
+
+                # Map seq to q, k, v.
+                q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+                attention = self.softmax(q @ k.T / (self.d_head ** 0.5))
+                seq_result.append(attention @ v)
+            result.append(torch.hstack(seq_result))
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+
+class MyViTBlock(nn.Module):
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(MyViTBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.mhsa = MyMSA(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
+
+    def forward(self, x):
+        # MHSA + residual connection.
+        out = x + self.mhsa(self.norm1(x))
+        # Feedforward + residual connection
+        out = out + self.mlp(self.norm2(out))
+        return out
 
 class MyViT(nn.Module):
     """
@@ -103,11 +207,37 @@ class MyViT(nn.Module):
         
         """
         super().__init__()
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+
+        self.chw = chw  # (C, H, W)
+        self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+
+        # Input and patches sizes
+        assert chw[1] % n_patches == 0  # Input shape must be divisible by number of patches
+        assert chw[2] % n_patches == 0
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)  ### WRITE YOUR CODE HERE
+
+        # Linear mapper
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+
+        # Learnable classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+
+        # Positional embedding
+        # HINT: don't forget the classification token
+        self.positional_embeddings = get_positional_embeddings(n_patches ** 2 + 1, hidden_d)  ### WRITE YOUR CODE HERE
+
+        # Transformer blocks
+        self.blocks = nn.ModuleList([MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+
+        # Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
 
     def forward(self, x):
         """
@@ -121,7 +251,30 @@ class MyViT(nn.Module):
         """
         ##
         ###
-        #### WRITE YOUR CODE HERE!
+        n, c, h, w = x.shape
+
+        # Divide images into patches.
+        patches = patchify(x, self.n_patches)
+
+        # Map the vector corresponding to each patch to the hidden size dimension.
+        tokens = self.linear_mapper(patches)
+
+        # Add classification token to the tokens.
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+
+        # Add positional embedding.
+        # HINT: use torch.Tensor.repeat(...)
+        preds = tokens + self.positional_embeddings.repeat(n, 1, 1)
+
+        # Transformer Blocks
+        for block in self.blocks:
+            preds = block(preds)
+
+        # Get the classification token only.
+        preds = preds[:, 0]
+
+        # Map to the output distribution.
+        preds = self.mlp(preds)
         ###
         ##
         return preds
